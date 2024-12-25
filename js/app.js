c4680966729bd4252a19c856d2724fd3f1ad660d
DerekHale1923 <107768236+DerekHale1923@users.noreply.github.com>
2024-12-25 15:43:16 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+const swiper = new Swiper('.swiper', {
+    speed: 1600,
+    /*spaceBetween: 300,            расстояние между слайдами */
+    mousewheel: true,            /* прокрутка колесом*/
+    direction: 'vertical',       /* направление переключения слайдов */
+    parallax: true,              /* добавляет функции параллакса */
+})
